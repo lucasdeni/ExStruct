7fed2538e1c3ac34fbd359473260f021d57bf385
Lucas Denipoti <lucasfdenipoti@gmail.com>
2020-10-21 22:16:48 -0300

    adicionei struct ao código do exercício dos properties

---
+++ ExStruct/Class1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace ExStruct
+{
+    struct Produto //Utilizando Struct ao invés de classe
+    {
+        //Declaração de variavéis privadas
+        private string _nome;
+        private double _preço;
+        private int _quantidade;
+
+        //Construtor personalizado
+        public Produto(string nome, double preço, int quantidade)
+        {
+            _nome = nome;
+            _preço = preço;
+            _quantidade = quantidade;
+        }
+
+        // Utiização de properties
+        public string Nome
+        {
+            get
+            {
+                return _nome;
+            }
+            set
+            {
+                if (value != null && value.Length > 1)
+                {
+                    _nome = value;
+                }
+            }
+        }
+        public double Preço
+        {
+            get
+            {
+                return _preço;
+            }
+        }
+        public int Quantidade
+        {
+            get
+            {
+                return _quantidade;
+            }
+        }
+        public double ValorTotalEmEstoque() //Vai definir o valor total do estoque
+        {
+            return _preço * _quantidade;
+        }
+        public void AdicionarProdutos(int quantidade) //Vai adicionar produtos no estoque
+        {
+            _quantidade += quantidade;
+        }
+        public void RemoverProdutos(int quantidade) //Vai remover produtos do estoque
+        {
+            _quantidade -= quantidade;
+        }
+        //Declaração da String
+        public override string ToString()
+        {
+            return _nome + ", R$ " + _preço.ToString("F2", CultureInfo.InvariantCulture) + ", " + _quantidade + " unidades, Total: R$ " + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
--- ExStruct/Program.cs
+++ ExStruct/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace ExStruct
 {
@@ -6,7 +7,33 @@ namespace ExStruct
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Produto p = new Produto("PS5", 6000.00, 10); //Instanciar o produto
+
+            //Inprimir as saídas
+            Console.WriteLine("Nome do Produto: " + p.Nome);
+            Console.WriteLine("Preço individual do Produto: R$" + p.Preço.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Quantidade do Produto: " + p.Quantidade);
+            Console.WriteLine("Valor Total do Estoque: R$" + p.ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture));
+
+            Console.Write("Digite o número de produtos a serem adicionados no estoque: ");
+            int qte = int.Parse(Console.ReadLine());
+            p.AdicionarProdutos(qte); // vai adicionar os produtos da quantidade
+
+            //Dados atualizados pós adição
+            Console.WriteLine("Nome do Produto: " + p.Nome);
+            Console.WriteLine("Quantidade do Produto: " + p.Quantidade);
+            Console.WriteLine("Valor Total do Estoque: R$" + p.ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture));
+
+            Console.Write("Digite o número de produtos a serem removidos no estoque: ");
+            qte = int.Parse(Console.ReadLine());
+            p.RemoverProdutos(qte); // vai remover os produtos da quantidade 
+
+            //Dados atualizados pós remoção
+            Console.WriteLine("Nome do Produto: " + p.Nome);
+            Console.WriteLine("Quantidade do Produto: " + p.Quantidade);
+            Console.WriteLine("Valor Total do Estoque: R$" + p.ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture));
+
+            Console.ReadKey();
         }
     }
 }
